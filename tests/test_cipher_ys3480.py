65ebdfd64c9cbc83cfed94b4d87a94ef50aeb64d
su ying <ying.su99a@gmail.com>
2021-11-05 13:47:28 -0400

    committing html documentation and test

--- tests/test_cipher_ys3480.py
+++ tests/test_cipher_ys3480.py
@@ -1,1 +1,12 @@
 from cipher_ys3480 import cipher_ys3480
+
+import pytest
+
+
+def test_cipher_word():
+    example = 'abstruse'
+    expected = 'bctusvtf'
+    actual = cipher_ys3480.cipher('abstruse', 1)
+    assert actual == expected
+
+test_cipher_word()
